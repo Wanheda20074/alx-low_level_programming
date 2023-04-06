1296b6bba007ee8ab0c43d31399b1d6ebfe53e61
wanheda420 <boukhanoufatimazahraa@gmail.com>
2023-04-05 18:22:03 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,13 +4,15 @@
  * @s:string
  * Return:int
  */
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return (0);
-	}
+	int i = 0;
 
-	s++;
-	return (_strlen_recurson(s) + 1);
-}	
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+
+	}
+	return (i);
+}

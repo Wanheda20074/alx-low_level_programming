d2db920e96007ee4f887c6ada1f4f91b05f568b0
wanheda420 <boukhanoufatimazahraa@gmail.com>
2023-04-12 15:13:41 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,13 +3,13 @@
 
 /**
  * create_array - creates an array of chars,
- * and  and initializes it with a specific char.
+ * and initializes it with a specific char.
  * @size: the size of the array
  * @c: initial value
  *
- * Returns: a pointer to the array, or NULL if it fails
+ * Return: a pointer to the array, or NULL if it fails
  */
- char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
@@ -18,11 +18,11 @@
 		return (NULL);
 
 	array = (char *)malloc(sizeof(char) * size);
-	if(array == NULL)
+	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		array[i] =c;
+		array[i] = c;
 
 	return (array);
 }
